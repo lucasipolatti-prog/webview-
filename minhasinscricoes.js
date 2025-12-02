65b280d3427947b679f9ba680ff1932cf6b2d9c8
ploeze <davipcdias@gmail.com>
2025-12-02 13:02:47 -0300

    Add files via upload

--- minhasinscricoes.js
+++ minhasinscricoes.js
@@ -1,17 +1,17 @@
-// 1. Pega os dados do LocalStorage
+// Pega os dados do LocalStorage
 let dadosSalvos = localStorage.getItem('minhasInscricoes');
 let corpoTabela = document.getElementById("tabela-corpo");
 
 if (dadosSalvos) {
-    // Transforma de texto para Lista de Objetos
+    
     let listaInscricoes = JSON.parse(dadosSalvos);
 
-    // Limpa a tabela antes de começar (para garantir)
+   
     corpoTabela.innerHTML = "";
 
-    // 2. Para cada item na lista, cria uma linha na tabela
+    
     listaInscricoes.forEach(function(item) {
-        // Cria o HTML da linha (tr) e das células (td)
+        
         let linha = `
             <tr>
                 <td>${item.nome}</td>
@@ -21,7 +21,7 @@ if (dadosSalvos) {
             </tr>
         `;
         
-        // Adiciona a linha dentro do corpo da tabela
+       
         corpoTabela.innerHTML += linha;
     });
 
